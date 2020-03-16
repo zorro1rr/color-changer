8181c608e4550bc2545d03508e0c3e6ee071d189
Justin Howard <zorro1rr@gmail.com>
2020-03-16 16:02:40 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Button } from './Button';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Random extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { color: [110, 79, 169] };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  componentDidMount() {
+    this.applyColor();
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentDidUpdate(prevProps, prevState) {
+    this.applyColor();
+  }
+
+  formatColor(ary) {
+    return 'rgb(' + ary.join(', ') + ')';
+  }
+
+  isLight() {
+    const rgb = this.state.color;
+    return rgb.reduce((a,b) => a+b) < 127 * 3;
+  }
+
+  applyColor() {
+    const color = this.formatColor(this.state.color);
+    document.body.style.background = color;
+  }
+
+  chooseColor() {
+    const random = [];
+    for (let i = 0; i < 3; i++) {
+      random.push(Math.floor(Math.random()*256));
+    }
+    return random;
+  }
+  
+  handleClick(){
+    this.setState({
+      color: this.chooseColor()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className={this.isLight() ? 'white' : 'black'}>
+          Your color is   {this.formatColor(this.state.color)}.
+        </h1>
+        <button light={this.isLight()} onClick={this.handleClick}> Press to change color
+          </ button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Random />, 
+  document.getElementById('app')
+);
