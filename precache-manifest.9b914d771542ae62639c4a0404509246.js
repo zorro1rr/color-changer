44339cbc853425a42ac14b8ebc8505612e27f0fc
Justin Howard <zorro1rr@gmail.com>
2020-03-20 23:15:47 -0600

    Updates

---
+++ precache-manifest.9b914d771542ae62639c4a0404509246.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a26d7ecf0a7b2ff58042b3f047458f",
+    "url": "/color-changer/index.html"
+  },
+  {
+    "revision": "465b4c1c1fee034936d7",
+    "url": "/color-changer/static/js/2.fd47f068.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/color-changer/static/js/2.fd47f068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de286afea37e9154385f",
+    "url": "/color-changer/static/js/main.0aceb051.chunk.js"
+  },
+  {
+    "revision": "d49ddd1766df83b73c22",
+    "url": "/color-changer/static/js/runtime-main.b4985870.js"
+  }
+]);
